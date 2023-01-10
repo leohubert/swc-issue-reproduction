1e6019b8ec9386f7b231b0d2c0fd3b987d7886ab
Léo Hubert <leo.hubert@bodyguard.ai>
2023-01-10 11:22:03 +0100

    add comment

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import addFunction from '@/utils/add'
+// import addFunction from '@/utils/add' // This works
 
 async function main() {
-    // const addFunction = (await import('@/utils/add')).default
+    const addFunction = (await import('@/utils/add')).default // This doesn't work
 
     console.log('2 + 3 =', addFunction(2, 3))
 }
